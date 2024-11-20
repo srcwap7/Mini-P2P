f2ef4a87268f43d827c51653d9a17d5912c7fdf5
srcwap7 <arpang@iitbhilai.ac.in>
2024-11-21 03:18:50 +0530

    committed second

--- server.cpp
+++ server.cpp
@@ -6,11 +6,13 @@
 #include <unistd.h>
 #include <bits/stdc++.h>
 #include <pthread.h>
+#include <dirent.h>
+
 
 #define SILLY_LISTENER_PORT 7999        
 #define SILLY_CLIENT_PORT 8000
 #define SILLY_TCP_PORT 8001
-#define SILLY_LISTENER_PORT 8002
+#define SILLY_LISTENER_TCP_PORT 8002
 
 
 #define BUF_SIZE 4096 
@@ -47,16 +49,63 @@ string generateHexadecimal(int length) {
     return ss.str();
 }
 
-int onLeaderElected(int sock,int sock2){
+int onLeaderElected(){
     sockaddr_in destAddr;
+    const char* request="SEND FILE LOG";
+    map<string,vector<string>> fileMap;
     for (auto it:clientMap){
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
         memset(&destAddr,0,sizeof(destAddr));
         destAddr.sin_family=AF_INET;
-        destAddr.sin_port=htons(SILLY_LISTENER_PORT);
+        destAddr.sin_port=htons(SILLY_LISTENER_TCP_PORT);
         const char* f=it.first.c_str();
         destAddr.sin_addr.s_addr=inet_addr(f);
-        
+        string ip=it.first.c_str();
+        if (inet_pton(AF_INET, ip.c_str(), &destAddr.sin_addr) <= 0) {
+            cerr << "Invalid IP address: " << ip << std::endl;
+            close(sock);
+            return -1;
+        }
+        if (connect(sock, (struct sockaddr*)&destAddr, sizeof(destAddr)) < 0) {
+            close(sock);
+            return -1;
+        }
+        if (send(sock, request, strlen(request), 0) < 0) {
+            perror("Send request failed");
+            close(sock);
+            return -1;
+        }
+        char buffer[BUF_SIZE];
+        int bytesReceived = recv(sock, buffer, BUF_SIZE - 1, 0);
+        if (bytesReceived < 0) {
+            perror("Receive failed");
+        }else{
+            buffer[bytesReceived] = '\0';
+            string response(buffer);
+            size_t pos = 0;
+            string delimiter = "\n";
+            while ((pos = response.find(delimiter)) != std::string::npos) {
+                std::string fileName = response.substr(0, pos);
+                response.erase(0, pos + delimiter.length());
+                fileMap[fileName].push_back(ip);
+            }
+        }
+        close(sock);
     }
+    std::ofstream outFile("filemap.txt");
+    if (!outFile.is_open()) {
+        std::cerr << "Error opening filemap.txt for writing." << std::endl;
+        return -1;
+    }
+    for (const auto& entry : fileMap) {
+        outFile << entry.first; 
+        for (const auto& ip : entry.second) {
+            outFile << " " << ip; 
+        }
+        outFile << "\n";
+    }
+    outFile.close();
+    std::cout << "File map written to filemap.txt successfully." << std::endl;
     return 0;
 }
 
@@ -71,19 +120,13 @@ void listen_for_broadcast(int sock,int sock2){
     socklen_t addrSize;
     int timer=0;
     while (1) {
-        cout << "Listening for broadcasts..." << endl;  
         addrSize = sizeof(clientAddr);
+        cout << "Listening for broadcasts..." << endl;  
         int bytesReceived = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr *)&clientAddr, &addrSize);
         if (bytesReceived < 0) DieWithError("recvfrom() failed");
-        else if (bytesReceived>0){
-            int rc=fork();
-            if (rc<0){
-                cerr<<"Error in forking"<<endl;
-                exit(EXIT_FAILURE);
-            }
-            else if (rc==0){
+        else if (bytesReceived>0 && strcmp(inet_ntoa(clientAddr.sin_addr),localIP)!=0){
                 const char* senderIP=inet_ntoa(clientAddr.sin_addr);
-                if (strcmp(buffer,"Hey! I am alone, Care to join me?")==0 && strcmp(senderIP,localIP)!=0){
+                if (strcmp(buffer,"Hey! I am alone, Care to join me?")==0){
                     cout<<"Received Broadcast"<<endl;
                     const char* ack_msg="Yes! I wanna join you";
                     clientAddr.sin_port=htons(SILLY_LISTENER_PORT);
@@ -99,19 +142,6 @@ void listen_for_broadcast(int sock,int sock2){
                         if (sendto(sock2,id_msg,strlen(id_msg),0,(struct sockaddr*)&clientAddr,sizeof(clientAddr))<0) DieWithError("Error in sending ID");
                         cout<<"New ID sent"<<endl;
                     }       
-                }
-                else if (starts_with(buffer,"Leader changed to")){
-                    char* trail="Leader changed to "; 
-                    char* newLeader=buffer+strlen(trail);
-                    if (strcmp(leader,localIP)!=0){
-                        leader=newLeader;
-                        int rc=onLeaderElected(sock,sock2);
-                        if (rc==0) cout<<"All good"<<endl;
-                        else cout<<"Error in leader election"<<endl;
-                    }
-                    else{
-                        leader=newLeader;
-                    }
                 }
                 else if (starts_with(buffer,"Your ID is: ") && !isConnected){
                     char* trail="Your ID is: ";
@@ -192,7 +222,7 @@ void listen_for_broadcast(int sock,int sock2){
                         leader=newLeader;
                         if (strcmp(leader,localIP)){
                             isLeader=true;
-                            int x = onLeaderElected(sock,sock2);
+                            int x = onLeaderElected();
                             if (x!=0) DieWithError("Error in itialising");
                         }
                     }
@@ -201,8 +231,6 @@ void listen_for_broadcast(int sock,int sock2){
                     char* trail="Leader At: ";
                     leader=buffer+strlen(trail);
                 }
-                exit(EXIT_SUCCESS);
-            }
         }
         buffer[bytesReceived] = '\0';
         printf("Received message from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
@@ -218,20 +246,110 @@ void send_broadcast(int sock,int sock2){
     broadcastAddr.sin_family=AF_INET;
     broadcastAddr.sin_port=htons(SILLY_LISTENER_PORT);
     broadcastAddr.sin_addr.s_addr=inet_addr(broadcastIP);
-    while (!isConnected){
+    int counter=0;
+    while (!isConnected && counter<3){
         if (sendto(sock,broadcast_msg,strlen(broadcast_msg),0,(struct sockaddr*)&broadcastAddr,sizeof(broadcastAddr))<0){
            DieWithError("Error in sending broadcast");
         }
-        cout<<"Sent Broadcast"<<endl;
+        cout<<"Sent Broadcast for connecting to request"<<endl;
         sleep(2);
+        counter++;
     }
-    exit(EXIT_SUCCESS);
+    if (counter==3){
+        /*THIS MEANS THERE IS NO OTHER ONE IN THE NETWORK*/
+        isLeader=true;
+        char temp_buffer[BUF_SIZE];
+        strcpy(temp_buffer,localIP);
+        leader=temp_buffer;
+        string my_id=generateHexadecimal(40);
+        myId=new char[my_id.size()+1];
+        strcpy(myId,my_id.c_str());
+        isConnected=true;
+        FILE* fptr=fopen("clientMap.txt","w");
+        fprintf(fptr,"%s %s\n",localIP,myId);
+        fclose(fptr);
+    }
 }
 
-void establish_TCP_connection(){
-    
+vector<string> listFilesInDirectory() {
+    std::vector<std::string> files;
+    DIR* dir = opendir(".");
+    if (dir == nullptr) {
+        perror("Unable to open directory");
+        return files;
+    }
+
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != nullptr) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            files.push_back(entry->d_name);
+        }
+    }
+    closedir(dir);
+    return files;
 }
 
+
+void handleClient(int clientSock, sockaddr_in clientAddr) {
+    char buffer[BUF_SIZE];
+    int bytesReceived = recv(clientSock, buffer, BUF_SIZE - 1, 0);
+    if (bytesReceived < 0) {
+        perror("Receive failed");
+        close(clientSock);
+        return;
+    }
+    buffer[bytesReceived] = '\0';
+    std::string senderIP = inet_ntoa(clientAddr.sin_addr);
+    std::cout << "Request from " << senderIP << ": " << buffer << std::endl;
+    if (senderIP == leader) {
+        std::vector<std::string> files = listFilesInDirectory();
+        std::string response = "Files in directory:\n";
+        for (const auto& file : files) {
+            response += file + "\n";
+        }
+        send(clientSock, response.c_str(), response.size(), 0);
+        std::cout << "Response sent to leader: " << senderIP << std::endl;
+    } else {
+        std::cout << "Request ignored: not from leader" << std::endl;
+    }
+
+    close(clientSock);
+}
+
+void listen_for_file_log(){
+    int serverSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSock < 0) {
+        perror("Socket creation failed");
+        return;
+    }
+
+    sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SILLY_LISTENER_TCP_PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(serverSock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Bind failed");
+        close(serverSock);
+        return;
+    }
+    if (listen(serverSock, 5) < 0) {
+        perror("Listen failed");
+        close(serverSock);
+        return;
+    }
+    while (true) {
+        sockaddr_in clientAddr;
+        socklen_t clientLen = sizeof(clientAddr);
+        int clientSock = accept(serverSock, (struct sockaddr*)&clientAddr, &clientLen);
+        if (clientSock < 0) {
+            perror("Accept failed");
+            continue;
+        }
+        handleClient(clientSock, clientAddr);
+    }
+    close(serverSock);
+}
 
 int main() {
     int sock;
@@ -262,9 +380,11 @@ int main() {
     if (setsockopt(sock2, SOL_SOCKET, SO_BROADCAST, &broadcastPermission, sizeof(broadcastPermission)) < 0) {
         DieWithError("Setting socket options failed");
     }
-     
+    cout<<"All sockets are set"<<endl;  
     thread t1(listen_for_broadcast,sock,sock2);
     thread t2(send_broadcast,sock,sock2);
+    thread t3(listen_for_file_log);
     t1.join();
     t2.join();
+    t3.join();
 }
